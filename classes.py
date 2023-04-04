e9b8e26437f23291bb4fa425cc41056a970dac0c
Maxwell Stull <maxwstull@gmail.com>
2023-04-04 16:02:14 -0500

    Added json saving and loading, and help message

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+import json
 
 class App():
     # Creation
@@ -7,18 +8,22 @@ class App():
         self.users[author.id] = User(author.id, author.name, author.nick)
     # Save / Load
     def load_json(self):
-        pass
+        fp = open("savedata.json","r")
+        to_load = json.load(fp)
+        for user_id, user in to_load.items():
+            self.users[user_id] = User().load_json(user)
+
     def save_json(self):
-        pass
+        to_save = {}
+        for user_id, user in self.users.items():
+            to_save[user_id] = user.save_json()
+        with open("savedata.json","w") as fp:
+            json.dump(to_save, fp)
     # Currency
     def add(self, author, message): 
         pass 
-    def add_complex(self, author, message):
-        pass 
     def remove(self, author, message):
         pass 
-    def remove_complex(self, author, message):
-        pass
     def get_balance(self, author):
         pass 
     def pay_player(self, author, message):
@@ -29,22 +34,39 @@ class App():
     def remove_ration(self, author, message):
         pass 
     # DM Comments
-
+    def next_day(self):
+        pass
+    def audit(self):
+        pass
+    # Misc
+    def print_user(self, id):
+        print(self.users[id])
         
 
 
 
 class User():
-    def __init__(self, id, name, nick) -> None:
+    def __init__(self, id=0, name="", nick="") -> None:
         self.id = id 
         self.name = name
         self.nick = nick
         self.currency = Money()
         self.rations = Ration()
-    def load_json(self):
-        pass 
+    def load_json(self, json_data):
+        self.id = json_data['id']
+        self.name = json_data['name']
+        self.nick = json_data['nick']
+        self.currency = Money().load_json(json_data['currency'])
+        self.rations = Ration().load_json(json_data['rations'])
     def save_json(self):
-        pass 
+        return {"id":self.id, "name":self.name, "nick":self.nick, "currency":self.currency.save_json(), "rations":self.rations.save_json()}
+    def __repr__(self) -> str:
+        return """
+        Name: {nm}
+        Nick: {nk}
+            Money: {m}
+            Ration: {r}
+        """.format(nm = self.name, nk=self.nick, m=str(self.currency), r=str(self.rations))
 
 class Money():
     def __init__(self) -> None:
@@ -54,6 +76,12 @@ class Money():
     def __repr__(self) -> str:
         retval = "[{pl}p,{gp}g,{cu}c]".format(pl=self.plat, gp=self.gold, cu=self.copper)
         return retval 
+    def save_json(self):
+        return {"p":self.plat, "g":self.gold, "c":self.copper}
+    def load_json(self, json_data):
+        self.copper = json_data['c']
+        self.gold = json_data['g']
+        self.plat = json_data['p']
     
 class Ration():
     def __init__(self) -> None:
@@ -62,3 +90,8 @@ class Ration():
     def __repr__(self) -> str:
         retval = "[{fd} meals, {wt} gal]".format(fd=self.food,wt=self.water)
         return retval
+    def save_json(self):
+        return {"meals":self.food, "water":self.water}
+    def load_json(self, json_data):
+        self.food = json_data['meals']
+        self.water = json_data['water']
--- main.py
+++ main.py
@@ -3,6 +3,39 @@ import discord
 from classes import App 
 from secret_token import Token
 
+def help_message():
+    return """
+Version 0.1 of econodrone, for tracking economy or economy-adjacent things in D&D.
+
+Player Commands:
+  ### GENERAL
+    //save - Saves all current active content to json
+    //me - prints user information
+  ### MONEY
+    //add --<unit> <amnt> - Adds money, unit is either 'p' for plat, 'g' for gold, or 'c' for copper.  
+        Example: //add --g 10   will add 10 gold
+        Example: //add --g 10 --p 1 will add 10 gold and 1 platinum
+    //remove --<unit> <amnt> - Removes money, see "add" for units
+        Example: Same formats as add
+    //balance - Bot will print out your current balance
+    //pay_player --<unit> <amnt> - Will spawn a context menu to choose player to send the money to
+        Example: //pay_player --g 10 --p 1
+  ### FOOD
+    //add_ration <amnt> - Adds rations to inventory
+        Example: //add_ration 4 will add 4 rations
+    //remove_ration <amnt> - removes rations from inventory
+        Example: No.
+    //add_water <amnt> - Adds water to inventory
+        Example: See above.
+    //drink_water <amnt> - Slurp slurp slurp slurp
+
+Sudo Commands (require elevated rights):
+    //load - Loads content from json, OVERWRITING whatever is there
+    //next_day - Removes 1 ration and 1 water from all
+    //audit - Prints all players //me's
+    """
+
+
 
 client = discord.Client(intents=discord.Intents.all())
 appy = App()
@@ -22,13 +55,17 @@ async def on_message(message):
         command = payload[0]
         match command:
             case "save":
-                print("Saving")
+                appy.save_json()
             case "load":
-                print("Loading")
+                appy.load_json()
+            case "me":
+                appy.print_user(message.author.id)
+            case "help":
+                await message.channel.send(help_message())
+                
 
-#    print(message)
+    print(message)
 #    print(message.author.id)
 #    print(message.author.name)
-tolken = Token().token
-client.run(tolken)
+client.run(Token().token)
 
